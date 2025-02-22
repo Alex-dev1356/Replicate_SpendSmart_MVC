d302ff35580498d39f9cf1cc98834f9dbe9e5fab
Alexander Soliven <asoliven26@yahoo.com>
2025-02-22 10:37:17 +0800

    Adding the necessarry Model on the Form, Create a DB to store data and adding CreateEditExpensesForm for forms

---
+++ Replacate_SpendSmart_MVC/Models/ExpensesModelDatabase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Replacate_SpendSmart_MVC.Models
+{
+    public class ExpensesModelDatabase : DbContext
+    {
+        public DbSet<ExpensesModel> ExpensesData { get; set; }
+
+        public ExpensesModelDatabase(DbContextOptions<ExpensesModelDatabase> options) 
+            : base(options)
+        {
+            
+        }
+
+    }
+}
