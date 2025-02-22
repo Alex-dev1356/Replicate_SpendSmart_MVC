cf1d08f0a6e287377ac2b78ce81ec269df29ccf4
Alexander Soliven <asoliven26@yahoo.com>
2025-02-22 08:02:11 +0800

    Creating Expenses Model for storing the data in the Db

---
+++ Replacate_SpendSmart_MVC/Models/ExpensesModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Replacate_SpendSmart_MVC.Models
+{
+    public class ExpensesModel
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        [Required]
+        public string Desription { get; set; }
+
+    }
+}
